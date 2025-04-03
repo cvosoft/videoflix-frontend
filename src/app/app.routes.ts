d329722c6e29a45de7d1282c95ff28bcf7d6d671
Christoph Völker <christov@gmx.de>
2025-04-03 15:54:09 +0200

    some routing basics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { StartpageComponent } from './startpage/startpage.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: StartpageComponent },
+    { path: 'legalnotice', component: LegalNoticeComponent },
+    { path: 'imprint', component: ImprintComponent }
+];
---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  imports: [RouterModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+
+}
