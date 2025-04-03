5d7e11ecfe334007335e386dfaa782e58f7103ef
Christoph Völker <christov@gmx.de>
2025-04-03 20:19:55 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,36 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./shared/footer/footer.component";
 import { HeaderComponent } from "./shared/header/header.component";
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'videoflix';
+  backgroundClass = "";
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const url = event.urlAfterRedirects;
+        if (url === '/') {
+          this.backgroundClass = 'startpage-bg';
+        } else if (url.startsWith('/legalnotice')) {
+          this.backgroundClass = 'legalnotice-bg';
+        } else if (url.startsWith('/imprint')) {
+          this.backgroundClass = 'imprint-bg';
+        } else {
+          this.backgroundClass = '';
+        }
+      });
+
+  }
+
 }
