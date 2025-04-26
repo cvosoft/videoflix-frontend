5fbce073cbb5c9c39ecc0884704041811cb73f86
Christoph Völker <christov@gmx.de>
2025-04-26 11:36:13 +0200

    update

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class SignUpComponent {
 
+  email: string = '';
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.email = params['email'] || '';
+    });
+  }
 }
--- src/app/startpage/startpage.component.ts
+++ src/app/startpage/startpage.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-startpage',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './startpage.component.html',
   styleUrl: './startpage.component.scss'
 })
 export class StartpageComponent {
+  email: string = '';
 
+  constructor(private router: Router) { }
+
+  onSubmit() {
+    if (this.email) {
+      this.router.navigate(['/register'], { queryParams: { email: this.email } });
+    }
+  }
 }
+
