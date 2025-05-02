36ec2af460aa7c6997ee87aaac2f4231b2427261
Christoph Völker <christov@gmx.de>
2025-05-02 21:48:14 +0200

    sign up update

--- src/app/startpage/startpage.component.ts
+++ src/app/startpage/startpage.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-startpage',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './startpage.component.html',
   styleUrl: './startpage.component.scss'
 })
