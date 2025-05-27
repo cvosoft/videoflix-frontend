5dae46cd0a44405f78abd3b475992ce54d9302ff
Christoph Völker <christov@gmx.de>
2025-05-27 19:44:43 +0200

    video offer

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -1,17 +1,18 @@
 // app/models/models.ts
 export interface Predigt {
-    id: number;
-    title: string;
-    video_file: string;
-    date: string;
-    thumbnail_file: string;
-    series_title: string;
-  }
-  
-  export interface Serie {
-    id: number;
-    title: string;
-    description: string;
-    thumbnail_file: string;
-    predigten: Predigt[];
-  }
+  id: number;
+  title: string;
+  video_file: string;
+  date: string;
+  thumbnail_file: string;
+  series_title: string;
+  description: string;
+}
+
+export interface Serie {
+  id: number;
+  title: string;
+  description: string;
+  thumbnail_file: string;
+  predigten: Predigt[];
+}
